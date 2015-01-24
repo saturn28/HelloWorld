bd7910c017efbc11ed31d47e9c1b968617c64157
saturn28 <jeffcroc28@gmail.com>
2015-01-24 10:56:44 -0500

    Initial version

---
+++ simon/defines.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+* defines.h
+* Author Jeff Crockett
+* Date 01/01/2015
+* Rev A
+* Description
+* This file contains all of the includes and defines for the Simon arduino 
+* project
+*******************************************************************************/
+#include <stdio.h>
+#include <SoftwareSerial.h>
+#include "pitches.h"
+#define STRB        2
+#define DBG_LED     13
+#define BAUDRATE    9600
+#define RANDOM_MAX  0x8000L
+#define SEED_MASK   0x7FFFL
+#define PLAYBACK_WAIT_TIME  500L
+#define BLANK_TIME  100L
+#define TONE_PIN     8
+
+#define DIAGNOSTIC 16    
+#define START      17 
+#define INPLAY     18
+#define PLAYBACK   19
+#define EXAMINEUSR 20
+#define WAITB4PLAYBACK  21
+
+    
+//#define TESTMODE  
+
+
+
+
